e0b8288bdfc75a421267fe870619497ef5733eb7
Yuta <y.sugiyama54@gmail.com>
2022-02-26 16:23:00 +0900

    CLUDメソッドのモックを追加

--- cmd/api/movie-handler.go
+++ cmd/api/movie-handler.go
@@ -19,8 +19,6 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("id is", id)
-
 	movie, err := app.models.DB.Get(id)
 	if err != nil {
 		app.errorJSON(w, err)
@@ -46,4 +44,20 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovie(w http.ResponseWriter, r *http.Request) {
+
 }
